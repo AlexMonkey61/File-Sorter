14c0ca1bd9cc3799401521a2ccead49c09b31429
AlexMonkey61 <alexneon4ik@gmail.com>
2026-01-05 09:44:22 +0300

    first code

---
+++ values.py
@@ -0,0 +1,14 @@
+mainDir = "C:/Users/Alex/Downloads"
+
+types = {
+    'image': ['.png', '.jpg', '.jpeg'],
+    'video': ['.mp4', '.mov'],
+    'audio': ['.mp3', '.m4a', '.wav'],
+    'text': ['.txt'],
+    'app': ['.exe'],
+    'archive': ['.zip', '.7zip', '.rar', '.tar.gz', '.tar.xz'],
+    'pdf': ['.pdf'],
+    'word': ['.docx'],
+    'powerpoint': ['.pptx'],
+    'excel': ['.xlsx']
+}
